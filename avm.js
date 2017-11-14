95dc4f0e53a475b9e5a80af9a533b254f28151d4
intro-swf <33427068+intro-swf@users.noreply.github.com>
2017-11-14 22:41:10 +0000

    Remove semicolon and change indentation

--- avm.js
+++ avm.js
@@ -19,28 +19,28 @@ define(['ReadableOp'], function(ReadableOp) {
   }
 
   op('GoToFrame', 0x81)
-  .assign({
-    writeBinary: function(out) {
-      out.u8(this.code).u16(2).u16(this.frameNumber);
-    },
-  })
-  .Typify(function GoToFrame(n) {
-    this.frameNumber = n;
-  });
+    .assign({
+      writeBinary: function(out) {
+        out.u8(this.code).u16(2).u16(this.frameNumber);
+      },
+    })
+    .Typify(function GoToFrame(n) {
+      this.frameNumber = n;
+    });
   op('GetURL', 0x83)
-  .assign({
-    writeBinary: function(out) {
-      out.u8(this.code).u16(this.url.length + 1 + this.target.length + 1)
-        .utf8(this.url).u8(0)
-        .utf8(this.target).u8(0);
-    },
-  })
-  .Typify(function GetURL(url, target) {
-    if (url.indexOf('\0') !== -1 || target.indexOf('\0') !== 0) {
-      throw new Error('strings may not contain null characters');
-    }
-    this.param('url', url, true).param('target', target, true);
-  });
+    .assign({
+      writeBinary: function(out) {
+        out.u8(this.code).u16(this.url.length + 1 + this.target.length + 1)
+          .utf8(this.url).u8(0)
+          .utf8(this.target).u8(0);
+      },
+    })
+    .Typify(function GetURL(url, target) {
+      if (url.indexOf('\0') !== -1 || target.indexOf('\0') !== 0) {
+        throw new Error('strings may not contain null characters');
+      }
+      this.param('url', url, true).param('target', target, true);
+    });
   op('NextFrame', 0x04);
   op('PreviousFrame', 0x05);
   op('Play', 0x06);
@@ -48,14 +48,14 @@ define(['ReadableOp'], function(ReadableOp) {
   op('ToggleQuality', 0x08);
   op('StopSounds', 0x09);
   op('WaitForFrame', 0x8A)
-  .assign({
-    writeBinary: function(out) {
-      out.u8(this.code).u16(3).u16(this.frameNumber).u8(this.skipCount);
-    },
-  });
-  .Typify(function WaitForFrame(frameNumber, skipCount) {
-    this.frameNumber = frameNumber;
-  });
+    .assign({
+      writeBinary: function(out) {
+        out.u8(this.code).u16(3).u16(this.frameNumber).u8(this.skipCount);
+      },
+    })
+    .Typify(function WaitForFrame(frameNumber, skipCount) {
+      this.frameNumber = frameNumber;
+    });
 
   return avm;
 
