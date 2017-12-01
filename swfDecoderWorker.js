d6d3d34b7de2116175c6c0eeccea6ad836d4f034
intro-swf <33427068+intro-swf@users.noreply.github.com>
2017-12-01 16:57:00 +0000

    Create swfDecoderWorker.js

---
+++ swfDecoderWorker.js
@@ -0,0 +1,20 @@
+
+importScripts('require.js');
+
+self.onmessage = function onmessage(e) {
+  var message, messages = JSON.decode(e.data);
+  for (var i_msg = 0; i_msg < messages.length; i_msg++) {
+    switch ((message = messages[i_msg])[0]) {
+      case 'open':
+        var url = message[1];
+        postMessage(JSON.stringify([
+          ['init', {count:1, rate:12, bounds:'0 0 200 200'}],
+          ['f'],
+        ]);
+        break;
+      // case 'import':
+      default:
+        throw new Error('unknown message: ' + message);
+    }
+  }
+};
