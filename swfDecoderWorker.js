9f017478d4da1aa267d6698ae65bde3b5cf99554
intro-swf <33427068+intro-swf@users.noreply.github.com>
2017-12-02 19:01:29 +0000

    Post ready message notification

--- swfDecoderWorker.js
+++ swfDecoderWorker.js
@@ -1,212 +1,219 @@
 
 importScripts('require.js');
-importScripts('dataExtensions.js', 'ChunkReader.js');
 
-require('dataExtensions');
+require(['dataExtensions'], function(_) {
+  
+  'use strict';
+  
+  importScripts('ChunkReader.js');
 
-const TAG_END = 0
-  ,TAG_SHOW_FRAME = 1
-  ,TAG_DEFINE_SHAPE = 2
-    ,TAG_DEFINE_SHAPE_2 = 22
-    ,TAG_DEFINE_SHAPE_3 = 32
-    ,TAG_DEFINE_SHAPE_4 = 83
-  ,TAG_PLACE_OBJECT = 4
-    ,TAG_PLACE_OBJECT_2 = 26
-  ,TAG_REMOVE_OBJECT = 5
-    ,TAG_REMOVE_OBJECT_2 = 28
-  // DEFINE_BITS tags always use long length?
-  ,TAG_DEFINE_BITS = 6
-    ,TAG_DEFINE_BITS_2 = 21
-    ,TAG_DEFINE_BITS_3 = 35
-    ,TAG_DEFINE_BITS_4 = 90
-    ,TAG_DEFINE_BITS_LOSSLESS = 20
-    ,TAG_DEFINE_BITS_LOSSLESS_2 = 36
-  ,TAG_DEFINE_BUTTON = 7
-    ,TAG_DEFINE_BUTTON_2 = 34
-  ,TAG_JPEG_TABLES = 8
-  ,TAG_SET_BACKGROUND_COLOR = 9
-  ,TAG_DEFINE_FONT = 10
-    ,TAG_DEFINE_FONT_2 = 48
-    ,TAG_DEFINE_FONT_INFO = 13
-    ,TAG_DEFINE_FONT_INFO_2 = 62
-    ,TAG_DEFINE_FONT_NAME = 88
-  ,TAG_DEFINE_TEXT = 11
-    ,TAG_DEFINE_TEXT_2 = 33
-  ,TAG_DO_ACTION = 12
-  ,TAG_DEFINE_SOUND = 14
-  ,TAG_PLAY_SOUND = 15
-  ,TAG_DEFINE_BUTTON_SOUND = 17
-  ,TAG_SOUND_STREAM_HEAD = 18
-    ,TAG_SOUND_STREAM_HEAD_2 = 45
-  ,TAG_SOUND_STREAM_BLOCK = 19
-  ,TAG_PROTECT = 24
-    ,TAG_ENABLE_DEBUGGER = 58
-    ,TAG_ENABLE_DEBUGGER_2 = 64
-  ,TAG_DEFINE_EDIT_TEXT = 37
-  ,TAG_DEFINE_SPRITE = 39
-  ,TAG_FRAME_LABEL = 43
-  ,TAG_DEFINE_MORPH_SHAPE = 46
-  ,TAG_FILE_ATTRIBUTES = 69
-  ,TAG_DEFINE_SCALING_GRID = 78
-  ,TAG_EXPORT = 56
-  ,TAG_DEBUG_ID = 63
-;
+  const TAG_END = 0
+    ,TAG_SHOW_FRAME = 1
+    ,TAG_DEFINE_SHAPE = 2
+      ,TAG_DEFINE_SHAPE_2 = 22
+      ,TAG_DEFINE_SHAPE_3 = 32
+      ,TAG_DEFINE_SHAPE_4 = 83
+    ,TAG_PLACE_OBJECT = 4
+      ,TAG_PLACE_OBJECT_2 = 26
+    ,TAG_REMOVE_OBJECT = 5
+      ,TAG_REMOVE_OBJECT_2 = 28
+    // DEFINE_BITS tags always use long length?
+    ,TAG_DEFINE_BITS = 6
+      ,TAG_DEFINE_BITS_2 = 21
+      ,TAG_DEFINE_BITS_3 = 35
+      ,TAG_DEFINE_BITS_4 = 90
+      ,TAG_DEFINE_BITS_LOSSLESS = 20
+      ,TAG_DEFINE_BITS_LOSSLESS_2 = 36
+    ,TAG_DEFINE_BUTTON = 7
+      ,TAG_DEFINE_BUTTON_2 = 34
+    ,TAG_JPEG_TABLES = 8
+    ,TAG_SET_BACKGROUND_COLOR = 9
+    ,TAG_DEFINE_FONT = 10
+      ,TAG_DEFINE_FONT_2 = 48
+      ,TAG_DEFINE_FONT_INFO = 13
+      ,TAG_DEFINE_FONT_INFO_2 = 62
+      ,TAG_DEFINE_FONT_NAME = 88
+    ,TAG_DEFINE_TEXT = 11
+      ,TAG_DEFINE_TEXT_2 = 33
+    ,TAG_DO_ACTION = 12
+    ,TAG_DEFINE_SOUND = 14
+    ,TAG_PLAY_SOUND = 15
+    ,TAG_DEFINE_BUTTON_SOUND = 17
+    ,TAG_SOUND_STREAM_HEAD = 18
+      ,TAG_SOUND_STREAM_HEAD_2 = 45
+    ,TAG_SOUND_STREAM_BLOCK = 19
+    ,TAG_PROTECT = 24
+      ,TAG_ENABLE_DEBUGGER = 58
+      ,TAG_ENABLE_DEBUGGER_2 = 64
+    ,TAG_DEFINE_EDIT_TEXT = 37
+    ,TAG_DEFINE_SPRITE = 39
+    ,TAG_FRAME_LABEL = 43
+    ,TAG_DEFINE_MORPH_SHAPE = 46
+    ,TAG_FILE_ATTRIBUTES = 69
+    ,TAG_DEFINE_SCALING_GRID = 78
+    ,TAG_EXPORT = 56
+    ,TAG_DEBUG_ID = 63
+  ;
 
-function readSWF(input) {
-  var frameCount;
-  function showFrame() {
-    frameCount--;
-    var f = new FrameInfo;
-    while (input.peekUint16LE() === 0x0040) {
-      input.skipBytes(2);
-      f.count++;
+  function readSWF(input) {
+    var frameCount;
+    function showFrame() {
       frameCount--;
+      var f = new FrameInfo;
+      while (input.peekUint16LE() === 0x0040) {
+        input.skipBytes(2);
+        f.count++;
+        frameCount--;
+      }
+      self.postMessage(JSON.stringify([f]));
     }
-    postMessage(JSON.stringify([f]));
-  }
-  function readChunkHeader() {
-    return input.gotUint16LE().then(function(b) {
-      var typeCode = b >>> 6;
-      var len = b & 0x3F;
-      if (len < 0x3F) {
-        if (len === 0) {
-          return processChunk(typeCode);
+    function readChunkHeader() {
+      return input.gotUint16LE().then(function(b) {
+        var typeCode = b >>> 6;
+        var len = b & 0x3F;
+        if (len < 0x3F) {
+          if (len === 0) {
+            return processChunk(typeCode);
+          }
+          return input.gotUint8Array(len).then(function(data) {
+            return processChunk(typeCode, data);
+          });
         }
-        return input.gotUint8Array(len).then(function(data) {
+        return input.gotUint32LE().then(function(len) {
+          return input.gotUint8Array(len);
+        })
+        .then(function(data) {
           return processChunk(typeCode, data);
         });
+      });
+    }
+    function processChunk(typeCode, data) {
+      switch (typeCode) {
+        case TAG_END:
+          if (frameCount > 0) {
+            if (frameCount === 1) {
+              showFrame();
+            }
+            else {
+              throw new Error('missing ' + frameCount + ' frames');
+            }
+          }
+          return;
+        case TAG_SHOW_FRAME:
+          showFrame();
+          break;
+        default:
+          //console.log('unhandled tag: ' + typeCode, data);
+          break;
       }
-      return input.gotUint32LE().then(function(len) {
-        return input.gotUint8Array(len);
+      return readChunkHeader();
+    }
+    input.gotUint8Array(8).then(function(bytes) {
+      switch (String.fromCharCode(bytes[0], bytes[1], bytes[2])) {
+        case 'CWS': input = input.makeInflateReader(); break;
+        case 'FWS': break;
+        default: throw new Error('invalid header');
+      }
+      var version = bytes[3];
+      var uncompressedFileSize = new DataView(bytes.buffer, bytes.byteOffset+4, 4).getUint32(0, true);
+      var frameBounds, framesPerSecond;
+      return input.gotSWFRect().then(function(rect) {
+        frameBounds = rect;
+        return input.gotUint16LE();
       })
-      .then(function(data) {
-        return processChunk(typeCode, data);
+      .then(function(fps) {
+        framesPerSecond = fps / 0x100;
+        return input.gotUint16LE();
+      })
+      .then(function(count) {
+        frameCount = count;
+        self.postMessage(JSON.stringify([['init', {
+          v: version,
+          bounds: frameBounds.toString(),
+          count: frameCount,
+          rate: framesPerSecond,
+        }]]));
+        return readChunkHeader();
       });
     });
   }
-  function processChunk(typeCode, data) {
-    switch (typeCode) {
-      case TAG_END:
-        if (frameCount > 0) {
-          if (frameCount === 1) {
-            showFrame();
-          }
-          else {
-            throw new Error('missing ' + frameCount + ' frames');
-          }
-        }
-        return;
-      case TAG_SHOW_FRAME:
-        showFrame();
-        break;
-      default:
-        //console.log('unhandled tag: ' + typeCode, data);
-        break;
+
+  self.onmessage = function onmessage(e) {
+    var message, messages = JSON.parse(e.data);
+    for (var i_msg = 0; i_msg < messages.length; i_msg++) {
+      switch ((message = messages[i_msg])[0]) {
+        case 'open':
+          var chunkReader = new ChunkReader;
+          chunkReader.fromURL(message[1]);
+          readSWF(chunkReader);
+          break;
+        // case 'import':
+        default:
+          throw new Error('unknown message: ' + message);
+      }
     }
-    return readChunkHeader();
-  }
-  input.gotUint8Array(8).then(function(bytes) {
-    switch (String.fromCharCode(bytes[0], bytes[1], bytes[2])) {
-      case 'CWS': input = input.makeInflateReader(); break;
-      case 'FWS': break;
-      default: throw new Error('invalid header');
-    }
-    var version = bytes[3];
-    var uncompressedFileSize = new DataView(bytes.buffer, bytes.byteOffset+4, 4).getUint32(0, true);
-    var frameBounds, framesPerSecond;
-    return input.gotSWFRect().then(function(rect) {
-      frameBounds = rect;
-      return input.gotUint16LE();
-    })
-    .then(function(fps) {
-      framesPerSecond = fps / 0x100;
-      return input.gotUint16LE();
-    })
-    .then(function(count) {
-      frameCount = count;
-      postMessage(JSON.stringify([['init', {
-        v: version,
-        bounds: frameBounds.toString(),
-        count: frameCount,
-        rate: framesPerSecond,
-      }]]));
-      return readChunkHeader();
-    });
+  };
+
+  Object.assign(ChunkReader.prototype, {
+    gotSWFRect: function() {
+      const reader = this, rect = new SWFRect;
+      var coordBits;
+      return this.gotTopBits(5).then(function(b) {
+        coordBits = b;
+        return reader.gotTopBits(coordBits, true);
+      })
+      .then(function(b) {
+        rect.left = b;
+        return reader.gotTopBits(coordBits, true);
+      })
+      .then(function(b) {
+        rect.right = b;
+        return reader.gotTopBits(coordBits, true);
+      })
+      .then(function(b) {
+        rect.top = b;
+        return reader.gotTopBits(coordBits, true);
+      })
+      .then(function(b) {
+        rect.bottom = b;
+        reader.flushBits();
+        return rect;
+      });
+    },
   });
-}
 
-self.onmessage = function onmessage(e) {
-  var message, messages = JSON.parse(e.data);
-  for (var i_msg = 0; i_msg < messages.length; i_msg++) {
-    switch ((message = messages[i_msg])[0]) {
-      case 'open':
-        var chunkReader = new ChunkReader;
-        chunkReader.fromURL(message[1]);
-        readSWF(chunkReader);
-        break;
-      // case 'import':
-      default:
-        throw new Error('unknown message: ' + message);
-    }
+  function SWFRect() {
   }
-};
+  SWFRect.prototype = {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    get width() { return this.right - this.left; },
+    get height() { return this.bottom - this.top; },
+    isEqualTo: function(r) {
+      if (this === r) return true;
+      return this.left === r.left && this.top === r.top
+          && this.right === r.right && this.bottom === r.bottom;
+    },
+    toString: function() {
+      return [this.left, this.top, this.right, this.bottom].join(' ');
+    },
+  };
 
-Object.assign(ChunkReader.prototype, {
-  gotSWFRect: function() {
-    const reader = this, rect = new SWFRect;
-    var coordBits;
-    return this.gotTopBits(5).then(function(b) {
-      coordBits = b;
-      return reader.gotTopBits(coordBits, true);
-    })
-    .then(function(b) {
-      rect.left = b;
-      return reader.gotTopBits(coordBits, true);
-    })
-    .then(function(b) {
-      rect.right = b;
-      return reader.gotTopBits(coordBits, true);
-    })
-    .then(function(b) {
-      rect.top = b;
-      return reader.gotTopBits(coordBits, true);
-    })
-    .then(function(b) {
-      rect.bottom = b;
-      reader.flushBits();
-      return rect;
-    });
-  },
+  function FrameInfo() {
+  }
+  FrameInfo.prototype = {
+    count: 1,
+    toJSON: function() {
+      var json = ['f'];
+      if (this.count !== 1) {
+        json.push(this.count);
+      }
+      return json;
+    },
+  };
+  
+  self.postMessage('[["ready"]]');
+  
 });
-
-function SWFRect() {
-}
-SWFRect.prototype = {
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  get width() { return this.right - this.left; },
-  get height() { return this.bottom - this.top; },
-  isEqualTo: function(r) {
-    if (this === r) return true;
-    return this.left === r.left && this.top === r.top
-        && this.right === r.right && this.bottom === r.bottom;
-  },
-  toString: function() {
-    return [this.left, this.top, this.right, this.bottom].join(' ');
-  },
-};
-
-function FrameInfo() {
-}
-FrameInfo.prototype = {
-  count: 1,
-  toJSON: function() {
-    var json = ['f'];
-    if (this.count !== 1) {
-      json.push(this.count);
-    }
-    return json;
-  },
-};
