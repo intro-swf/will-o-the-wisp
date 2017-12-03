70e242c3a6645d3c32da063c212dd1717975633f
intro-swf <33427068+intro-swf@users.noreply.github.com>
2017-12-03 15:20:27 +0000

    Create url for shape SVG

--- swfDecoderWorker.js
+++ swfDecoderWorker.js
@@ -108,9 +108,11 @@ function(
           return;
         case TAG_DEFINE_SHAPE:
           var svg = new MakeshiftXML('svg', {xmlns:'http://www.w3.org/2000/svg'});
-          var g = svg.el('g', {id:3});
+          var id = 'shape' + source.readUint16LE();
+          var g = svg.el('g', {id:id});
           g.el('path', {d:'m0,0h50v50h-50v-50'});
-          console.log(svg.toString());
+          var url = URL.createObjectURL(svg.toBlob({type:'image/svg+xml'}))+'#'+id;
+          console.log(svg.toString(), url);
           break;
         case TAG_SHOW_FRAME:
           showFrame();
