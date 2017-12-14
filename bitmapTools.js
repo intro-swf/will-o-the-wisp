f60ba1caa3695c6ab98d0aa2e60b6ae166771188
intro-swf <33427068+intro-swf@users.noreply.github.com>
2017-12-14 22:00:26 +0000

    Add makeBitmapBlob

--- bitmapTools.js
+++ bitmapTools.js
@@ -1,4 +1,4 @@
-define(['dataExtensions'], function(dataExtensions) {
+define(['dataExtensions', 'z'], function(dataExtensions, z) {
 
   'use strict';
   
@@ -125,6 +125,112 @@ define(['dataExtensions'], function(dataExtensions) {
     if (jpeg2 instanceof Blob) jpeg2 = jpeg2.slice(2);
     else jpeg2 = jpeg2.subarray(2);
     return new Blob([jpeg1, jpeg2], {type:'image/jpeg'});
+  };
+  
+  bitmapTools.makeBitmapBlob = function makeBitmapBlob(def) {
+    switch (def.bpp) {
+      case 1: case 2: case 4: case 8:
+        if (!def.palette) {
+          throw new Error('palette must be defined at ' + def.bpp + 'bpp');
+        }
+        break;
+      case 16: case 24: case 32:
+        if (def.palette) {
+          throw new Error('palette cannot be defined for ' + def.bpp + 'bpp');
+        }
+        break;
+      default:
+        throw new Error('invalid bpp value');
+    }
+    
+    var parts = [new Uint8Array([0x89]), 'PNG\r\n\x1A\n'];
+
+    function chunk(name, buf) {
+      var info = new ArrayBuffer(8);
+      var lenDV = new DataView(info, 0, 4);
+      var crcDV = new DataView(info, 4, 4);
+      var crc = new Uint8Array([
+        name.charCodeAt(0),
+        name.charCodeAt(1),
+        name.charCodeAt(2),
+        name.charCodeAt(3),
+      ]).getCRC32();
+      if (typeof buf.byteLength === 'number') {
+        lenDV.setUint32(0, buf.byteLength);
+        if (!(buf instanceof Uint8Array)) {
+          buf = new Uint8Array(buf.buffer, buf.byteOffset, buf.byteLength);
+        }
+        crcDV.setUint32(0, buf.getCRC32(crc));
+        parts.push(lenDV, name, buf, crcDV);
+      }
+      else {
+        var len = 0;
+        parts.push(lenDV, name);
+        for (var i = 0; i < buf.length; i++) {
+          var part = buf[i];
+          len += buf[i].byteLength;
+          if (!(part instanceof Uint8Array)) {
+            part = new Uint8Array(part.buffer, part.byteOffset, part.byteLength);
+          }
+          crc = part.getCRC32(crc);
+          parts.push(part);
+        }
+        lenDV.setUint32(0, len);
+        crcDV.setUint32(0, crc);
+        parts.push(crcDV);
+      }
+    }
+
+    const rows = def.rows;
+    
+    {
+      var IHDR = new DataView(new ArrayBuffer(13));
+      var width = ('width' in def) ? def.width : rows[0].length * 8 / def.bpp;
+      var height = rows.length;
+      IHDR.setUint32(0, width);
+      IHDR.setUint32(4, height);
+      IHDR.setUint8(8, def.bpp);
+      IHDR.setUint8(9,
+        (def.palette ? 1 : 0)
+        | 2 // color used
+        | (def.bpp === 32 && !def.ignoreAlpha ? 4 : 0) // tRNS not represented here
+      );
+      chunk('IHDR', IHDR);
+    }
+
+    if (def.palette) {
+      const palette = def.palette;
+      var PLTE = new Uint8Array(3 * palette.length);
+      var tRNS = new Uint8Array(palette.length);
+      var palbytes = new Uint8Array(palette.buffer, palette.byteOffset, palette.byteLength);
+      var top_tRNS = -1;
+      for (var i = 0; i < palette.length; i++) {
+        PLTE[i*3 + 0] = palbytes[i*4 + 0];
+        PLTE[i*3 + 1] = palbytes[i*4 + 1];
+        PLTE[i*3 + 2] = palbytes[i*4 + 2];
+        if ((tRNS[i] = palbytes[i*4 + 3]) !== 0xff) {
+          top_tRNS = i;
+        }
+      }
+      chunk('PLTE', PLTE);
+      if (top_tRNS >= 0) {
+        chunk('tRNS', tRNS.subarray(0, top_tRNS + 1));
+      }
+    }
+
+    {
+      // all rows must be prepended with filter byte
+      var rowBytes = 1 + rows[0].length;
+      var IDAT = new Uint8Array(rows.length * rowBytes);
+      for (var i = 0; i < rows.length; i++) {
+        IDAT.set(rows[i], i*rowBytes + 1);
+      }
+      IDAT = IDAT.toZStoredParts();
+      chunk('IDAT', IDAT);
+    }
+
+    chunk('IEND', new Uint8Array(0));
+    return new Blob(parts, {type:'image/png'});
   };
   
   return bitmapTools;
