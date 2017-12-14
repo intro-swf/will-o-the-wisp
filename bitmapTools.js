2d71e7ec354d99cfcf91e3bfcdaf16a9b9d6f3f1
intro-swf <33427068+intro-swf@users.noreply.github.com>
2017-12-14 19:36:20 +0000

    Check for reset markers

--- bitmapTools.js
+++ bitmapTools.js
@@ -91,10 +91,13 @@ define(['dataExtensions'], function(dataExtensions) {
           throw new Error('unexpected end of JPEG data');
         }
         for (;;) {
-          if (data.readUint8() === 0xFF && data.readUint8() !== 0) {
-            data.offset -= 2;
-            break;
+          if (data.readUint8() !== 0xFF) continue;
+          var marker = data.readUint8();
+          if (marker === 0 || (marker >= MARKER_RST0 && marker <= MARKER_RST7)) {
+            continue;
           }
+          data.offset -= 2;
+          break;
         }
         continue;
       
