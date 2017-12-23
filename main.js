b64fa908a7dcf6d0c662a39176a45e7e8c029d85
intro-swf <33427068+intro-swf@users.noreply.github.com>
2017-12-23 15:17:10 +0000

    Use correct co-ordinate value

--- main.js
+++ main.js
@@ -137,7 +137,7 @@ require([
         def.style.display = 'none';
         def.style.position = 'absolute';
         def.style.left = def.viewBox.baseVal.x + 'px';
-        def.style.top = def.viewBox.baseVal.x + 'px';
+        def.style.top = def.viewBox.baseVal.y + 'px';
         def.addEventListener('display-list-instantiate', function(e) {
           var displayList = e.detail.displayList;
           displayList.container.addEventListener('clean', this.onclean.bind(e.detail.displayObject));
