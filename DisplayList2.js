c10ff1559cbbc9cb9a52307ac6f86ffd153f7116
intro-swf <33427068+intro-swf@users.noreply.github.com>
2017-12-25 04:14:51 +0000

    Create DisplayList2.js

---
+++ DisplayList2.js
@@ -0,0 +1,199 @@
+define(['arrayExtensions'], function() {
+
+  'use strict';
+  
+  const COMPARE_DEPTH = function(a, b) {
+    if (typeof a !== 'number') a = a.depth;
+    if (typeof b !== 'number') b = b.depth;
+    return a - b;
+  };
+  
+  const COMPARE_NUM = function(a, b) {
+    return a - b;
+  };
+  
+  function getPrevEl(container, refEl) {
+    if (refEl) return refEl.previousElementSibling;
+    return container.lastElementChild;
+  }
+  
+  function addPrevEl(container, refEl, newEl) {
+    if (refEl) container.insertBefore(newEl, refEl);
+    else container.appendChild(newEl);
+  }
+
+  function DisplayList(root, displayObjectTemplates) {
+    this.rootElement = root;
+    this.displayObjectTemplates = displayObjectTemplates;
+    this.displayObjects = [];
+  }
+  DisplayList.prototype = {
+    setStateAt: function(depth, state, i_obj) {
+      if (depth < 0) {
+        this.rootElement.state = state;
+        this.rootElement.dispatchEvent(new Event('display-object-state'));
+        return;
+      }
+      if (isNaN(i_obj)) {
+        i_obj = this.displayObjects.sortedIndexOf(depth, COMPARE_DEPTH);
+      }
+      var existing;
+      if (i_obj < 0) {
+        existing = null;
+        i_obj = ~i_obj;
+      }
+      else {
+        existing = this.displayObjects[i_obj];
+      }
+      if (state) {
+        var displayObject;
+        if (!existing || existing.state.template !== state.template) {
+          var template = this.displayObjectTemplates[state.template];
+          if (!template) {
+            throw new Error('template not found: ' + state.template);
+          }
+          displayObject = template.cloneNode(true);
+          template.dispatchEvent(new Event('display-object-init', {
+            detail: {
+              displayList: this,
+              displayObject: displayObject,
+            },
+          }));
+          if (existing) {
+            existing.dispatchEvent(new Event('display-object-delete'));
+            this.displayObjects[i_obj] = displayObject;
+            existing.parentNode.insertBefore(displayObject, existing);
+            existing.parentNode.removeChild(existing);
+          }
+          else {
+            var container, insertBefore;
+            if (i_obj === this.displayObjects.length) {
+              insertBefore = null;
+              container = this.rootElement;
+            }
+            else {
+              insertBefore = this.displayObjects[i_obj];
+              container = insertBefore.parentNode;
+              while (container.startDepth > depth) {
+                insertBefore = container;
+                container = container.parentNode;
+              }
+            }
+            var prevEl;
+            while (prevEl = getPrevEl(container, insertBefore)) {
+              if (prevEl.stopDepth > depth) {
+                insertBefore = null;
+                container = prevEl;
+              }
+              else break;
+            }
+            addPrevEl(container, prevEl, displayObject);
+            this.displayObjects.splice(i_obj, 0, displayObject);
+          }
+        }
+        else {
+          displayObject = existing;
+        }
+        displayObject.state = state;
+        displayObject.dispatchEvent(new Event('display-object-state'));
+      }
+      else if (existing) {
+        this.displayObjects.splice(i_obj, 1);
+        existing.dispatchEvent(new Event('display-object-delete'));
+        existing.parentNode.removeChild(existing);
+      }
+    },
+    setAllStates: function(states) {
+      var depths = Object.keys(states).map(parseInt);
+      depths.sort(COMPARE_NUM);
+      var i_obj = 0, i_depth = 0;
+      while (i_obj < this.displayObjects.length) {
+        var displayObject = this.displayObjects[i_obj];
+        var depth = depths[i_depth];
+        if (isNaN(depth)) depth = Infinity;
+        if (depth < displayObject.depth) {
+          this.setStateAt(depth, states[depth], ~i_obj);
+          i_depth++;
+        }
+        else if (depth > displayObject.depth) {
+          this.setStateAt(displayObject.depth, null, i_obj);
+        }
+        else {
+          this.setStateAt(depth, states[depth], i_obj);
+          i_obj++;
+          i_depth++;
+        }
+      }
+      while (i_depth < depths.length) {
+        var depth = depths[i_depth];
+        this.setStateAt(depth, states[depth], ~i_obj++);
+      }
+    },
+    makeNodeGroup: function(groupTemplate, startDepth, stopDepth) {
+      var containerNodes = [];
+      var i_node = this.displayObjects.sortedIndexOf(startDepth, COMPARE_DEPTH);
+      if (i_node < 0) i_node = ~i_node;
+      var container, insertBefore;
+      if (i_node === this.displayObjects.length) {
+        insertBefore = null;
+        container = this.element;
+      }
+      else {
+        insertBefore = this.displayObjects[i_node];
+        container = insertBefore.parentNode;
+      }
+      while (container.startDepth === startDepth && container.stopDepth < stopDepth) {
+        insertBefore = container;
+        container = container.parentNode;
+      }
+      mainLoop: for (;;) {
+        var group = groupTemplate.cloneNode(true);
+        group.startDepth = startDepth;
+        containerNodes.push(group);
+        addPrevEl(container, insertBefore, group);
+        var nextEl;
+        while (nextEl = group.nextElementSibling) {
+          if (nextEl.depth >= stopDepth || nextEl.startDepth >= stopDepth) {
+            group.stopDepth = stopDepth;
+            break mainLoop;
+          }
+          group.appendChild(container.removeChild(group.nextElementSibling));
+        }
+        if (isNaN(container.stopDepth) || container.stopDepth >= stopDepth) {
+          group.stopDepth = stopDepth;
+          break mainLoop;
+        }
+        startDepth = group.stopDepth = container.stopDepth;
+        do {
+          insertBefore = container.nextElementSibling;
+          container = container.parentNode;
+        } while (container.stopDepth === startDepth);
+        while (insertBefore && insertBefore.startDepth === startDepth && insertBefore.stopDepth > stopDepth) {
+          container = insertBefore;
+          insertBefore = container.firstChild;
+        }
+      }
+      return new DisplayListGroup(this, containerNodes);
+    },
+  };
+  
+  function DisplayListGroup(displayList, containerNodes) {
+    this.displayList = displayList;
+    this.containerNodes = containerNodes;
+  }
+  DisplayListGroup.prototype = {
+    revoke: function() {
+      for (var i = 0; i < this.containerNodes.length; i++) {
+        var node = this.containerNodes[i];
+        var parentNode = node.parentNode;
+        while (node.firstChild) {
+          parentNode.insertBefore(node.removeChild(node.firstChild), node);
+        }
+        parentNode.removeChild(node);
+      }
+    },
+  };
+
+  return DisplayList;
+  
+});
