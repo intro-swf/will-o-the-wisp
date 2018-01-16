1c71605c970924461ff0141192648d19fb4c726a
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-16 01:56:11 +0000

    Log class filenames

--- midlet-main.js
+++ midlet-main.js
@@ -4,6 +4,15 @@ requirejs.config({
 });
 
 require(['java', 'z'], function(java, z) {
+  
+  function loadFiles(files) {
+    for (var filename in files) {
+      if (/\.class$/i.test(filename)) {
+        console.log(filename);
+      }
+    }
+  }
+  
   var xhr = new XMLHttpRequest;
   xhr.responseType = 'arraybuffer';
   xhr.open('GET', '//cors.archive.org/cors/misc_midlet/DoomRPG_s60v2-N70.jar');
@@ -11,6 +20,7 @@ require(['java', 'z'], function(java, z) {
     var jar = new Uint8Array(this.response);
     var dv = new DataView(this.response);
     var pos = 0;
+    var files = {};
     while (pos < jar.length) {
       if (dv.getUint32(pos, true) !== 0x04034b50) break;
       var flags = dv.getUint16(pos + 6, true);
@@ -46,8 +56,10 @@ require(['java', 'z'], function(java, z) {
         case 0: uncompressed = compressed; break;
         case 8: uncompressed = z.inflateRaw(compressed); break;
       }
-      console.log(filename, compressionMethod, compressed.length, uncompressed.length, uncompressedLen);
+      if (uncompressed.length !== uncompressedLen) throw new Error('bad decompression');
+      files[filename] = uncompressed;
     }
+    loadFiles(files);
   };
   xhr.send();
 });
