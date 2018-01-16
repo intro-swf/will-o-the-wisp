9eec77feae372b5e475c1c213e7b818d012b1962
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-16 00:50:50 +0000

    Grab and log jar

--- midlet-main.js
+++ midlet-main.js
@@ -3,6 +3,13 @@ requirejs.config({
   timeout: Infinity,
 });
 
-require(['java'], function(java) {
-  console.log(java);
+require(['java', 'z'], function(java, z) {
+  var xhr = new XMLHttpRequest;
+  xhr.responseType = 'arraybuffer';
+  xhr.open('GET', '//cors.archive.org/cors/midlet_misc/doom_rpg_mobile.jar');
+  xhr.onload = function(e) {
+    var jar = this.response;
+    console.log(jar);
+  };
+  xhr.send();
 });
