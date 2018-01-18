5f539c714c39e2a3119e84b7ff43016d3f54f7b4
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-18 03:12:02 +0000

    Object: Use new API

--- jlib/java/lang/Object.js
+++ jlib/java/lang/Object.js
@@ -5,39 +5,39 @@ define(['java'], function(java) {
   const JObject = java.Object;
   
   const _CTOR = java._CTOR;
-  const _HASHCODE = new Symbol('hashCode');
+  const _HASHCODE = Symbol('hashCode');
   
-  Object.assign(JObject.prototype, {
-    equals: function(o) {
-      return this === o;
-    },
-    finalize: function(){},
-    hashCode: function() {
-      if (_HASHCODE in this) {
-        return this[_HASHCODE];
-      }
-      return this[_HASHCODE] = (Math.random() * 0xffffffff) | 0;
-    },
-    toString: function() {
-      return this[_CTOR].name + "@" + (this.hashCode() >>> 0).toString(16);
-    },
-    getClass: function() {
-      return this[_CTOR].classObject;
-    },
-    clone: function() {
-      throw new Exception('NYI');
-    },
-    notify: function() {
-      throw new Exception('NYI');
-    },
-    notifyAll: function() {
-      throw new Exception('NYI');
-    },
-    wait: function() {
-      throw new Exception('NYI');
+  return java.define('java.lang.Object', {
+    base: null,
+    constructor: [java.Object],
+    methods: {
+      equals: [{ret:'boolean'}, 'object', function(other) {
+        return (this === other);
+      }],
+      getClass: function() {
+        return this[_CTOR].classObject;
+      },
+      hashCode: [{ret:'i32'}, function() {
+        if (_HASHCODE in this) {
+          return this[_HASHCODE];
+        }
+        return this[_HASHCODE] = (Math.random() * 0xffffffff) | 0;
+      }],
+      notify: [function() {
+      }],
+      notifyAll: [function() {
+      }],
+      toString: [{ret:'string'}, function() {
+        return this[_CTOR].name + "@" + (this.hashCode() >>> 0).toString(16);
+      }],
+      wait: [
+        [],
+        ['i64'],
+        ['i64', 'i32'],
+        function(timeout, nanos) {
+        },
+      ],
     },
   });
-
-  return JObject;
-
+  
 });
