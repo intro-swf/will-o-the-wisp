6eaeadee3a4f5d7c7b033ffc8d4ad6e4dce95b8f
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-18 03:24:16 +0000

    Class: Use new API

--- jlib/java/lang/Class.js
+++ jlib/java/lang/Class.js
@@ -5,129 +5,49 @@ define(['java'], function(java) {
   const _REFLECTS = java._REFLECTS;
   const _CTOR = java._CTOR;
 
-  Object.assign(Class.prototype, {
-    isInterface: function() {
-      return this[_REFLECTS] instanceof Interface;
+  return java.define('java.lang.Class', {
+    constructor: [{access:'private'}, Class],
+    staticMethods: {
+      forName: [{ret:Class}, 'string', function(className) {
+      }],
     },
-    isArray: function() {
-      return !!this[_REFLECTS].componentType;
-    },
-    isPrimitive: function() {
-      return !!this[_REFLECTS].isPrimitive;
-    },
-    getName: function() {
-      return this[_REFLECTS].name;
-    },
-    toString: function() {
-      var reflect = this[_REFLECTS];
-      if (reflect.isPrimitive) return reflect.name;
-      if (reflect instanceof Interface) return 'interface ' + reflect.name;
-      return 'class ' + reflect.name;
-    },
-    isInstance: function(v) {
-      var reflect = this[_REFLECTS];
-      if (reflect instanceof Interface) {
-        return v[_CTOR].interfaceSet.has(reflect);
-      }
-      return v instanceof this[_REFLECTS];
-    },
-    isAssignableFrom: function(cls) {
-      var other = cls[_REFLECTS];
-      if (other instanceof Interface) {
-        return this[_REFLECTS].interfaceSet.has(other);
-      }
-      return this[_REFLECTS].prototype instanceof other;
-    },
-    getSuperclass: function() {
-      var superclass = this[_REFLECTS].superclass;
-      if (!superclass) return null;
-      return superclass.classObject;
-    },
-    getInterfaces: function() {
-      return new Class.Array((this[_REFLECTS].interfaces || []).slice());
-    },
-    getSigners: function() {
-      return new java.Object.Array((this[_REFLECTS].signers || []).slice());
-    },
-    getComponentType: function() {
-      var componentType = this[_REFLECTS].componentType;
-      if (!componentType) return null;
-      return componentType.classObject;
-    },
-    getDeclaringClass: function() {
-      return null;
-    },
-    getClasses: function() {
-      return new Class.Array([]);
-    },
-    newInstance: function() {
-      //throws InstantiationException, IllegalAccessException
-      const TClass = this[_REFLECTS];
-      return new TClass();
-    },
-    getClassLoader: function() {
-      throw new Error('NYI');
-    },
-    getModifiers: function() {
-      throw new Error('NYI');
-    },
-    getFields: function() {
-      throw new Error('NYI');
-    },
-    getMethods: function() {
-      throw new Error('NYI');
-    },
-    getConstructors: function() {
-      throw new Error('NYI');
-    },
-    getField: function(name) {
-      throw new Error('NYI');
-    },
-    getMethod: function(name, parameterTypes) {
-      throw new Error('NYI');
-    },
-    getConstructor: function(parameterTypes) {
-      throw new Error('NYI');
-    },
-    getDeclaredClasses: function() {
-      throw new Error('NYI');
-    },
-    getDeclaredFields: function() {
-      throw new Error('NYI');
-    },
-    getDeclaredMethods: function() {
-      throw new Error('NYI');
-    },
-    getDeclaredConstructors: function() {
-      throw new Error('NYI');
-    },
-    getDeclaredField: function(name) {
-      throw new Error('NYI');
-    },
-    getDeclaredMethod: function(name, parameterTypes) {
-      throw new Error('NYI');
-    },
-    getDeclaredConstructor: function(parameterTypes) {
-      throw new Error('NYI');
-    },
-    getResourceAsStream: function(name) {
-      throw new Error('NYI');
-    },
-    getResource: function(name) {
-      // returns URL
-      throw new Error('NYI');
+    methods: {
+      getName: [{ret:'string'}, function() {
+        return this[_REFLECTS].name;
+      }],
+      getResourceAsStream: [{ret:'../io/InputStream'}, 'string', function(name) {
+      }],
+      isArray: [{ret:'boolean'}, function() {
+        return !!this[_REFLECTS].componentType;
+      }],
+      isAssignableFrom: [{ret:'boolean'}, Class, function(cls) {
+        var other = cls[_REFLECTS];
+        if (other instanceof Interface) {
+          return this[_REFLECTS].interfaceSet.has(other);
+        }
+        return this[_REFLECTS].prototype instanceof other;
+      }],
+      isInstance: [{ret:'boolean'}, 'object', function(v) {
+        var reflect = this[_REFLECTS];strin
+        if (reflect instanceof Interface) {
+          return v[_CTOR].interfaceSet.has(reflect);
+        }
+        return v instanceof this[_REFLECTS];
+      }],
+      isInterface: [{ret:'boolean'}, function() {
+        return this[_REFLECTS] instanceof Interface;
+      }],
+      newInstance: [{ret:'object'}, function() {
+        var TClass = this[_REFLECTS];
+        return new TClass();
+      }],
+      toString: [{ret:'string'}, function() {
+        var reflect = this[_REFLECTS];
+        if (reflect.isPrimitive) return reflect.name;
+        if (reflect instanceof Interface) return 'interface ' + reflect.name;
+        return 'class ' + reflect.name;
+      }],
     },
   });
   
-  Class.staticMembers = {
-    forName: function(className) {
-      throw new Error('NYI');
-    },
-    getPrimitiveClass: function(className) {
-      throw new Error('NYI');
-    },
-  };
-  
-  return Class;
-
 });
