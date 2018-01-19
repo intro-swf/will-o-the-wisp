c2e4a39db20153e3e66265e8c5a601e716b43ca6
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-19 18:05:40 +0000

    Create Light.js

---
+++ jlib/com/siemens/mp/game/Light.js
@@ -0,0 +1,16 @@
+define(['java'], function(java) {
+
+  'use strict';
+
+  return java.define('com.siemens.mp.game.Light', {
+    constructor: [function Light() {
+    }],
+    staticMethods: {
+      setLightOff: [function setLightOff() {
+      }],
+      setLightOn: [function setLightOn() {
+      }],
+    },
+  });
+
+});
