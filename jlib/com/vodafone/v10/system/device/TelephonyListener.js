3f8641a0d89c1bf9680aa8c67b90d1e0b6a6aa9f
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-19 20:04:35 +0000

    Create TelephonyListener.js

---
+++ jlib/com/vodafone/v10/system/device/TelephonyListener.js
@@ -0,0 +1,13 @@
+define(['java'], function(java) {
+
+  'use strict';
+
+  return java.define('com.vodafone.v10.system.device.ScheduledAlarmListener', {
+    base: 'interface',
+    methods: {
+      ring: [],
+      hangUp: [],
+    },
+  });
+
+});
