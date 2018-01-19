6cc5e83b6adf44b9497c2b2ea264207e83e9a580
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-19 17:32:06 +0000

    Create Util3D.js

---
+++ jlib/com/mascotcapsule/micro3d/v3/Util3D.js
@@ -0,0 +1,16 @@
+define(['java'], function(java) {
+
+  'use strict';
+  
+  return java.define('com.mascotcapsule.micro3d.v3.Util3D', {
+    staticMethods: {
+      cos: [{ret:'i32'}, 'i32', function cos(x) {
+      }],
+      sin: [{ret:'i32'}, 'i32', function sin(x) {
+      }],
+      sqrt: [{ret:'i32'}, 'i32', function sqrt(x) {
+      }],
+    },
+  });
+
+});
