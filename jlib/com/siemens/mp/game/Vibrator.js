9ad1a6e2f69a12deaf9e1227fc264c790f631ba4
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-19 19:04:15 +0000

    Create Vibrator.js

---
+++ jlib/com/siemens/mp/game/Vibrator.js
@@ -0,0 +1,18 @@
+define(['java'], function(java) {
+
+  'use strict';
+  
+  return java.define('com.siemens.mp.game.Vibrator', {
+    constructor: [function Vibrator() {
+    }],
+    staticMethods: {
+      startVibrator: [function startVibrator() {
+      }],
+      stopVibrator: [function stopVibrator() {
+      }],
+      triggerVibrator: ['i32', function triggerVibrator(duration) {
+      }],
+    },
+  });
+
+});
