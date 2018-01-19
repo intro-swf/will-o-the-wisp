40e1298398bfc4252ed806ac395dd55d403fea09
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-19 16:56:39 +0000

    Create Figure.js

---
+++ jlib/com/mascotcapsule/micro3d/v3/Figure.js
@@ -0,0 +1,29 @@
+define(['java'], function(java) {
+
+  'use strict';
+
+  return java.define('com.mascotcapsult.micro3d.v3.Figure', {
+    constructor: [
+      ['i8[]', function Figure(data) {
+      }],
+      ['string', function Figure(resourceName) {
+      }],
+    ],
+    methods: {
+      setPosture: [
+        './ActionTable', 'i32', 'i32',
+        function setPosture(actionTable, action, frame) {
+        },
+      ],
+      setTexture: [
+        ['./Texture', function setTexture(texture) {
+        }],
+        ['./Texture[]', function setTexture(textures) {
+        }],
+      ],
+      dispose: [function() {
+      }],
+    },
+  });
+
+});
