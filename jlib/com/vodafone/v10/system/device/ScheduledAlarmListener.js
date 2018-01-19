f5db76fac97e3e543acb6ecfed31bc73cca339be
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-19 20:04:06 +0000

    Create ScheduledAlarmListener.js

---
+++ jlib/com/vodafone/v10/system/device/ScheduledAlarmListener.js
@@ -0,0 +1,12 @@
+define(['java'], function(java) {
+
+  'use strict';
+
+  return java.define('com.vodafone.v10.system.device.ScheduledAlarmListener', {
+    base: 'interface',
+    methods: {
+      notice: ['string'],
+    },
+  });
+
+});
