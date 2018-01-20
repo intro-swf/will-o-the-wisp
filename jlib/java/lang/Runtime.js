981c74407920d86479559b4e6b66c81ab7c164f4
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-20 00:43:03 +0000

    Create Runtime.js

---
+++ jlib/java/lang/Runtime.js
@@ -0,0 +1,27 @@
+define(['java'], function(java) {
+
+  'use strict';
+  
+  return java.define('java.lang.Runtime', {
+    staticMethods: {
+      getRuntime: [{ret:'./Runtime'}, function() {
+        return java.currentThread.vm.runtimeObject;
+      }],
+    },
+    methods: {
+      exit: [{blocking:true}, 'i32', function exit(statusCode) {
+        return java.currentThread.vm.exit(statusCode);
+      }],
+      freeMemory: [{ret:'i64'}, function freeMemory() {
+        return java.currentThread.vm.freeMemory();
+      }],
+      gc: [function gc() {
+        java.currentThread.vm.gc();
+      }],
+      totalMemory: [{ret:'i64'}, function totalMemory() {
+        return java.currentThread.vm.totalMemory();
+      }],
+    },
+  });
+
+});
