220a55f3ffb7091259f36d7ed9e12995f36ca83c
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-21 17:52:44 +0000

    Update Image.js

--- jlib/javax/microedition/lcdui/Image.js
+++ jlib/javax/microedition/lcdui/Image.js
@@ -1,43 +1,64 @@
 define(['java'], function(java) {
-
-  function Image() {
-  }
   
-  java.initClass(Image, {
-    name: 'javax.microedition.lcdui.Image',
-    instanceMembers: {
-      getGraphics: function() {
-        throw new Error('NYI');
-      },
-      getHeight: function() {
-        throw new Error('NYI');
-      },
-      getRGB: function(out_argbArray, offset, scanLength, x,y, width,height) {
-        throw new Error('NYI');
-      },
-      getWidth: function() {
-        throw new Error('NYI');
-      },
-      isMutable: function() {
-        throw new Error('NYI');
-      },
+  'use strict';
+
+  return java.define('javax.microedition.lcdui.Image', {
+    final: true, // no accessible constructor
+    staticMethods: {
+      createImage: [
+        [
+          {ret:'./Image'}, 'i32', 'i32',
+          function createImage(width, height) {
+            // the only way to create a mutable image
+          },
+        ],
+        [
+          {ret:'./Image'}, './Image',
+          function createImage(copy) {
+          },
+        ],
+        [
+          './Image', 'i32', 'i32', 'i32', 'i32', 'i32',
+          function createImage(copy, x,y,width,height, transform) {
+          },
+        ],
+        [
+          {blocking:true}, 'i8[]', 'i32', 'i32',
+          function createImage(bytes, offset, length) {
+          },
+        ],
+        [
+          {blocking:true}, 'java.io.InputStream',
+          function createImage(inputStream) {
+          },
+        ],
+        [
+          {blocking:true}, 'string',
+          function createImage(resourceName) {
+          },
+        ],
+      ],
+      createRGBImage: [
+        {ret:'./Image'}, 'i32[]', 'i32', 'i32', 'boolean',
+        function(argbArray, width, height, processAlpha) {
+        },
+      ],
     },
-    staticMembers: {
-      createImage: function() {
-        // (bytes, offset, length)
-        // (image)
-        // (image, x, y, width, height, transform)
-        // (inputStream)
-        // (width, height)
-        // (resourceName)
-        throw new Error('NYI');
-      },
-      createRGBImage: function(argbArray, width, height, processAlpha) {
-        throw new Error('NYI');
-      },
+    methods: {
+      getGraphics: [{ret:'./Graphics'}, function getGraphics() {
+      }],
+      getWidth: [{ret:'i32'}, function getWidth() {
+      }],
+      getHeight: [{ret:'i32'}, function getHeight() {
+      }],
+      isMutable: [{ret:'boolean'}, function isMutable() {
+      }],
+      getRGB: [
+        {blocking:true}, 'i32[]', 'i32', 'i32', 'i32', 'i32', 'i32', 'i32',
+        function getRGB(out_argbArray, offset, scanLength, x,y, width,height) {
+        },
+      }],
     },
   });
-  
-  return Image;
 
 });
