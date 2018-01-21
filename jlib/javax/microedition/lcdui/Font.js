3202ee60b90de8f82c2fbff7afa93e2e32dc8aca
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-01-21 17:12:15 +0000

    Update Font.js

--- jlib/javax/microedition/lcdui/Font.js
+++ jlib/javax/microedition/lcdui/Font.js
@@ -2,76 +2,117 @@ define(['java'], function(java) {
 
   'use strict';
   
-  return java.defineClass(
+  const STYLE_PLAIN = 0,
+        STYLE_BOLD = 1,
+        STYLE_ITALIC = 2,
+        STYLE_UNDERLINED = 4,
+        SIZE_SMALL = 8,
+        SIZE_MEDIUM = 0,
+        SIZE_LARGE = 16,
+        FACE_SYSTEM = 0,
+        FACE_MONOSPACE = 32,
+        FACE_PROPORTIONAL = 64,
+        FONT_STATIC_TEXT = 0,
+        FONT_INPUT_TEXT = 1;
   
-    function Font() {
+  return java.define('javax.microedition.lcdui.Font', {
+    final: true,
+    constructor: [
+      {access:'private'},
+      function Font() {
+      },
+    ],
+    staticMethods: {
+      getDefaultFont: ['./Font', function getDefaultFont() {
+      }],
+      getFont: [
+        ['i32', function getFont(specifier) {
+        }],
+        ['i32','i32','i32', function getFont(face, style, size) {
+        }],
+      ],
     },
-    
-    {
-      name: 'javax.microedition.lcdui.Font',
-      instanceMembers: {
-        charsWidth: function(charArray, offset, length) {
-          throw new Error('NYI');
+    methods: {
+      charsWidth: [
+        {ret:'i32'}, 'char[]', 'i32', 'i32',
+        function charsWidth(chars, offset, length) {
         },
-        charWidth: function(char) {
-          throw new Error('NYI');
+      ],
+      charWidth: [
+        {ret:'i32'}, 'char',
+        function charWidth(c) {
         },
-        getBaselinePosition: function() {
-          throw new Error('NYI');
+      ],
+      getBaselinePosition: [
+        {ret:'i32'},
+        function getBaselinePosition() {
         },
-        getFace: function() {
-          throw new Error('NYI');
+      ],
+      getFace: [
+        {ret:'i32'},
+        function getFace() {
         },
-        getHeight: function() {
-          throw new Error('NYI');
+      ],
+      getHeight: [
+        {ret:'i32'},
+        function getHeight() {
         },
-        getSize: function() {
-          throw new Error('NYI');
+      ],
+      getSize: [
+        {ret:'i32'},
+        function getSize() {
         },
-        getStyle: function() {
-          throw new Error('NYI');
+      ],
+      getStyle: [
+        {ret:'i32'},
+        function getStyle() {
         },
-        isBold: function() {
-          throw new Error('NYI');
+      ],
+      isBold: [
+        {ret:'boolean'},
+        function isBold() {
         },
-        isItalic: function() {
-          throw new Error('NYI');
+      ],
+      isItalic: [
+        {ret:'boolean'},
+        function isItalic() {
         },
-        isPlain: function() {
-          throw new Error('NYI');
+      ],
+      isPlain: [
+        {ret:'boolean'},
+        function isPlain() {
         },
-        isUnderlined: function() {
-          throw new Error('NYI');
+      ],
+      isUnderlined: [
+        {ret:'boolean'},
+        function isUnderlined() {
         },
-        stringWidth: function(str) {
-          throw new Error('NYI');
+      ],
+      stringWidth: [
+        {ret:'i32'}, 'string',
+        function stringWidth(s) {
         },
-        substringWidth: function(str, offset, len) {
-          throw new Error('NYI');
+      ],
+      substringWidth: [
+        {ret:'i32'}, 'string', 'i32', 'i32',
+        function substringWidth(str, offset, len) {
         },
-      },
-      staticMembers: {
-        getDefaultFont: function() {
-          throw new Error('NYI');
-        },
-        getFont: function() {
-          // (specifier)
-          // (face, style, size)
-          throw new Error('NYI');
-        },
-        STYLE_PLAIN: 0,
-        STYLE_BOLD: 1,
-        STYLE_ITALIC: 2,
-        STYLE_UNDERLINED: 4,
-        SIZE_SMALL: 8,
-        SIZE_MEDIUM: 0,
-        SIZE_LARGE: 16,
-        FACE_SYSTEM: 0,
-        FACE_MONOSPACE: 32,
-        FACE_PROPORTIONAL: 64,
-        FONT_STATIC_TEXT: 0,
-        FONT_INPUT_TEXT: 1,
-      },
-    });
+      ],
+    },
+    constants: {
+      STYLE_PLAIN: STYLE_PLAIN,
+      STYLE_BOLD: STYLE_BOLD,
+      STYLE_ITALIC: STYLE_ITALIC,
+      STYLE_UNDERLINED: STYLE_UNDERLINED,
+      SIZE_SMALL: SIZE_SMALL,
+      SIZE_MEDIUM: SIZE_MEDIUM,
+      SIZE_LARGE: SIZE_LARGE,
+      FACE_SYSTEM: FACE_SYSTEM,
+      FACE_MONOSPACE: FACE_MONOSPACE,
+      FACE_PROPORTIONAL: FACE_PROPORTIONAL,
+      FONT_STATIC_TEXT: FONT_STATIC_TEXT,
+      FONT_INPUT_TEXT: FONT_INPUT_TEXT,
+    },
+  });
 
 });
