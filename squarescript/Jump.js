1d957c166556ccab91e204a831814c180a7d68bb
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-02-26 12:28:09 +0000

    Update Jump.js

--- squarescript/Jump.js
+++ squarescript/Jump.js
@@ -18,7 +18,34 @@ define(['./Step'], function(Step) {
   
   Jump.LABEL = Step.get('jump.label');
   
-  Jump.GUARD = Step.get('jump.guard');
+  Jump.MODE_NONE = 0;
+  Jump.MODE_FORWARDS = 1;
+  Jump.MODE_BACKWARDS = 2;
+  Jump.MODE_BIDIRECTIONAL = 3;
+  
+  function JumpGuard(mode) {
+    if (arguments.length === 0) {
+      mode = 3;
+    }
+    if (isNaN(mode) || mode !== mode & 3) {
+      throw new Error('invalid mode');
+    }
+    this.mode = mode;
+    Object.freeze(this);
+  }
+  JumpGuard.prototype = Object.create(Step.prototype);
+  Object.assign(JumpGuard.prototype, {
+    toJSON: function() {
+      switch (this.mode) {
+        case 0: return ['jump.guard', 'none'];
+        case 1: return ['jump.guard', 'forwards'];
+        case 2: return ['jump.guard', 'backwards'];
+        case 3: return ['jump.guard'];
+      }
+    },
+  });
+  
+  Jump.GUARD_NONE = new JumpGuard(Jump.MODE_NONE);
   
   return Jump;
 
